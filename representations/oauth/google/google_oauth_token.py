02da819bfcdf15abe2956d82778017f4d1bee395
Jordane Thomas <thomasjordane7@gmail.com>
2021-09-14 17:29:10 -0400

    bug fixes

--- representations/oauth/google/google_oauth_token.py
+++ representations/oauth/google/google_oauth_token.py
@@ -9,9 +9,9 @@ from .google_scope_map import GoogleScopeMap
 @dataclass
 class GoogleOAuthToken:
     access_token: str
-    refresh_token: str
     token_type: TokenType
     expires_in: timedelta
+    refresh_token: Optional[str] = None
     scopes: GoogleScopeMap = field(default_factory=GoogleScopeMap)
 
     created_on: datetime = field(default_factory=datetime.utcnow)
